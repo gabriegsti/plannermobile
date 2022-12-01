62f46416c929cb54d842d204208ad839ba750b45
gabriegsti <gabrielgsdevbr@gmail.com>
2022-12-01 16:49:24 -0300

    projeto final

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 
 const instance = {
+    //Essa url pertence ao ngrok por isso é preciso gerar uma nova toda vez que rodar o app com o expo. 
     baseURL: 'https://ca4d-187-67-186-47.sa.ngrok.io',
 };
 export default instance;
